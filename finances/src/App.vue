97ac2426ac90ad33590763edebeaa1cbe01efaf2
Lucas de Souza <lucasdesouza@Air-de-Lucas.lan>
2023-08-22 23:18:32 -0300

    validations

--- finances/src/App.vue
+++ finances/src/App.vue
@@ -2,9 +2,10 @@
   <!-- <cards></cards> -->
   <div>
     <h1>Teste appp</h1>
+    <Cards />
   </div>
 </template>
 
 <script setup>
-// import Cards from "./components/Cards.vue";
+import Cards from "./components/Cards.vue";
 </script>
